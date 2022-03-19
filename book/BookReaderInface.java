a6ccf79ab842dd83779d28bdb976c3a1d8c036a3
Gun Maksapsin <gun_102544@hotmail.com>
2022-03-19 14:29:33 +0700

    lab7softar

--- book/BookReaderInface.java
+++
@@ -1,5 +0,0 @@
-package book;
-
-public interface BookReaderInface {
-    void printToScreen();
-}
--- lsp/ASquare.java
+++
@@ -1,8 +0,0 @@
-package lsp;
-
-import lsp.Rectangle;
-import lsp.Square;
-
-public interface ASquare {
-    public int getArea() ;
-}
